0a791ea4655053e3010c69a4ca87d2d927f75dc3
Fabricio Paiva <40863175+fabricio24530@users.noreply.github.com>
2019-12-28 15:03:44 -0300

    Create 1008.py

---
+++ 1008.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+NUMBER = int(input())
+HORAS = int(input())
+SALARY = float(input())
+total = SALARY * HORAS
+print('NUMBER = {}\nSALARY = U$ {:.2f}'.format(NUMBER, total))
